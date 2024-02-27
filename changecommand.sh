d4487e522823cc550823516ef36ccc776361b615
recluze <recluze@gmail.com>
2024-02-27 09:42:16 +0300

    Coupons 24-C

--- changecommand.sh
+++ changecommand.sh
@@ -1,2 +1,2 @@
-grep -rl 'PAST' ./ | LC_ALL=C xargs sed -i '' 's/PAST-STUDENTS-24-A/PAST-STUDENTS-24-A/g'
+grep -rl 'PAST' ./ | LC_ALL=C xargs sed -i '' 's/PAST-STUDENTS-24-C/PAST-STUDENTS-24-C/g'
 # git add -u  
